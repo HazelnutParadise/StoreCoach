0f35a39bfa47f87cd5bcf2bd5f511e7d165fbafb
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-02-26 23:56:25 +0800

    新增 ReviewData 結構及 SaveToBuf 函數，並更新路由以儲存評論資料

--- app/app.go
+++ app/app.go
@@ -1,6 +1,21 @@
 package app
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/google/uuid"
+)
 
 var DataBuf sync.Map
 var ResultBuf sync.Map
+
+type ReviewData struct {
+	StoreName string   `json:"store_name"`
+	Reviews   []string `json:"reviews"`
+}
+
+func SaveToBuf(reviewData ReviewData) (dataUUID string) {
+	dataUUID = uuid.New().String()
+	DataBuf.Store(dataUUID, reviewData)
+	return
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,16 +1,23 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"StoreCoach/app"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetRoutes(r *gin.Engine) {
-	// Set routes here
 	r.POST("/", func(c *gin.Context) {
-		// TODO: 取得 json 資料，給 uuid，存入 DataBuf
+		// 取得 json 資料，給 uuid，存入 DataBuf
+		reviewData := app.ReviewData{}
+		c.BindJSON(&reviewData)
+		dataUUID := app.SaveToBuf(reviewData)
 		c.JSON(200, gin.H{
-			"message": "Hello, World!",
+			"data_uuid": dataUUID,
 		})
 	})
 
+	// TODO
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, World!",
