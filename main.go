ba6c26cc3559991d68685c9592bbdcf3eb083211
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-03-06 23:59:53 +0800

    更新 main.go，調整環境變數檢查邏輯，確保正確載入 .env 檔案

--- main.go
+++ main.go
@@ -3,23 +3,20 @@ package main
 import (
 	"StoreCoach/routes"
 	"log"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func init() {
-	if os.Getenv("GEMINI_API_KEY") == "" {
-		err := godotenv.Load("StoreCoach.env")
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
+	err := godotenv.Load("StoreCoach.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
 	}
 }
 
 func main() {
-	// gin.SetMode(gin.ReleaseMode)
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	routes.SetRoutes(r)
 	r.Run(":8080")
