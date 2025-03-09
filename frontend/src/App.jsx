feba11439c17ede070d2b79d669f7817de0e98dc
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-03-09 23:52:26 +0800

    調整 App.jsx 中的匯入語句格式以統一風格

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import {useEffect, useRef, useState } from "react";
 import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import {
